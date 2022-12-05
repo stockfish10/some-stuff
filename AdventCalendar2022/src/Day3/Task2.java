f0b89be45e44d07a8e6533b07f4e57340201c382
Borislav <77554605+stockfish10@users.noreply.github.com>
2022-12-05 21:02:44 +0200

    Add files via upload
    Day 3, both tasks completed

--- AdventCalendar2022/src/Day3/Task2.java
+++ AdventCalendar2022/src/Day3/Task2.java
@@ -15,6 +15,36 @@ public class Task2 {
 
         List<String> list = br.lines().collect(Collectors.toList());
 
+        int value = 0;
 
+        for (int i = 0; i < list.size(); i += 3) {
+            String firstBackpack = list.get(i);
+            String secondBackpack = list.get(i + 1);
+            String thirdBackpack = list.get(i + 2);
+
+            char badge = findCurrentBadge(firstBackpack, secondBackpack, thirdBackpack);
+
+
+            if (badge == Character.toUpperCase(badge)) {
+                value += (int) badge - 38;
+            } else {
+                value += (int) badge - 96;
+            }
+        }
+        System.out.println(value);
+    }
+
+    private static char findCurrentBadge(String firstBackpack, String secondBackpack, String thirdBackpack) {
+        char myChar = Character.MIN_VALUE;
+
+        for (int i = 0; i < firstBackpack.length(); i++) {
+            char current = firstBackpack.charAt(i);
+
+            if (secondBackpack.contains(String.valueOf(current)) && thirdBackpack.contains(String.valueOf(current))) {
+                myChar = current;
+            }
+        }
+
+        return myChar;
     }
 }
